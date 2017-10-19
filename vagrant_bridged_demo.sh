ee6bac48a0327aa9132f32eb193f60a5321cfdbb
ReSearchITEng <ReSearchITEng@gmail.com>
2017-10-19 11:01:43 +0300

    add ntpd, improvements to vagrant (to use fqdn)

--- vagrant_bridged_demo.sh
+++ vagrant_bridged_demo.sh
@@ -19,8 +19,16 @@ while [ $# -gt 0 ]; do
   ;;
   --bridged_adapter)
     HOST_BRIDGED_INTERFACE=$2
-  shift
-  shift
+    shift
+    shift
+  ;;
+  --restart)
+    ACTIONS=restart
+    shift
+  ;;
+  --regenerate_config)
+    ACTIONS=regenerate_config
+    shift
   ;;
  esac
 done
@@ -29,51 +37,89 @@ done
 VAGRANT_LOG=info # debug,info,warn,error
 #vagrant up
 
-###  Stop all machines for reconfiguration:
-for runningVagrM in $(vagrant status | grep 'running (virtualbox)' | cut -d" " -f1); do
-  vagrant halt $runningVagrM
-done
-
-
-### Get list of all already powered off machines:
-filter_machines_offvagm=$(vagrant status | grep 'poweroff (virtualbox)' | cut -d" " -f1)
-
-if [ "${filter_machines_offvagm}x" = "x" ]; then
- echo "There is no machine to manage, exit now"
- exit 1 # && error_now
-fi
-
-### Get list of machines created for local Vagrantfile in the current directory
-filter_machines_local_directory=$(basename `pwd`)  # note: cannot run it from /
-
-###
-echo "### Reconfigure the interfaces, disabling the NAT and making first interface bridged with "
-#set -vx
-for vagrantM in ${filter_machines_offvagm}; do
- for vboxM in $(VBoxManage list vms | grep -v inaccessible | grep $vagrantM | grep $filter_machines_local_directory | cut -d'"' -f2  ); do
-  #VBoxManage showvminfo $M | grep -i nic    #"--machinereadable"
-  VBoxManage modifyvm $vboxM --nic1 none --nic2 none --nic3 none --nic4 none --nic5 none --nic6 none --nic7 none --nic8 none 
-  VBoxManage modifyvm $vboxM --nic1 bridged --bridgeadapter1 $HOST_BRIDGED_INTERFACE --nictype1 82540EM --macaddress1 auto
-  #VBoxManage modifyvm $vboxM --nic2 nat --nictype2 82540EM --macaddress2 auto --natnet2 "10.0.2.0/24" --natpf2 "ssh,tcp,127.0.0.1,2222,,22" --natdnsproxy2 off --natdnshostresolver2 off # This is optional
-  VBoxManage startvm $vboxM --type headless  # DO NOT USE 'vagrant up', use VBoxManage startvm command
+if [ "${ACTIONS}x" != "regenerate_configx" ];then
+ ###  Stop all machines for reconfiguration and/or restart, but not when we only want to regenerate config
+ for runningVagrM in $(vagrant status | grep 'running (virtualbox)' | cut -d" " -f1); do
+   vagrant halt $runningVagrM
  done
-done
+
+ ### Get list of all already powered off machines:
+ filter_machines_offvagm=$(vagrant status | grep 'poweroff (virtualbox)' | cut -d" " -f1)
+
+ if [ "${filter_machines_offvagm}x" = "x" ]; then
+   echo "There is no machine to manage, exit now"
+   exit 1 # && error_now
+ fi
+
+ ### Get list of machines created for local Vagrantfile in the current directory
+ filter_machines_local_directory=$(basename `pwd`)  # note: cannot run it from /
+
+ ### If the only request is restart, do it and exit
+ if [ "${ACTIONS}" = "restartx" ];then
+  echo "### Restart VMs"
+  for vagrantM in ${filter_machines_offvagm}; do
+   for vboxM in $(VBoxManage list vms | grep -v inaccessible | grep $vagrantM | grep $filter_machines_local_directory | cut -d'"' -f2  ); do
+    VBoxManage startvm $vboxM --type headless  # DO NOT USE 'vagrant up', use VBoxManage startvm command
+   done
+  done
+  vagrant status
+  echo "Start vm triggered (via VBoxManage startvm). Once up, proceed with login using ssh -F ssh_config <host>; to check status use: vagrant status"
+  exit 0
+ fi
+
+ ###
+ echo "### Reconfigure the interfaces, disabling the NAT and making first interface bridged with "
+ #set -vx
+ for vagrantM in ${filter_machines_offvagm}; do
+  for vboxM in $(VBoxManage list vms | grep -v inaccessible | grep $vagrantM | grep $filter_machines_local_directory | cut -d'"' -f2  ); do
+    #VBoxManage showvminfo $M | grep -i nic    #"--machinereadable"
+    VBoxManage modifyvm $vboxM --nic1 none --nic2 none --nic3 none --nic4 none --nic5 none --nic6 none --nic7 none --nic8 none 
+    VBoxManage modifyvm $vboxM --nic1 bridged --bridgeadapter1 $HOST_BRIDGED_INTERFACE --nictype1 82540EM --macaddress1 auto
+    #VBoxManage modifyvm $vboxM --nic2 nat --nictype2 82540EM --macaddress2 auto --natnet2 "10.0.2.0/24" --natpf2 "ssh,tcp,127.0.0.1,2222,,22" --natdnsproxy2 off --natdnshostresolver2 off # This is optional
+    VBoxManage startvm $vboxM --type headless  # DO NOT USE 'vagrant up', use VBoxManage startvm command
+  done
+ done
+
+ ###
+ echo "  Machines were reconfigured and restarted. This is the vagrant status for virtuabox machines:"
+ vagrant status | grep '(virtualbox)'
+
+fi # Up to here we did actions when if [ "${ACTIONS}" != "regenerate_configx" ]
 
 ###
-echo "Machines were reconfigured and restarted. This is the vagrant status for virtuabox machines:"
-vagrant status | grep '(virtualbox)'
-
-###
-echo "### Generating a ssh_config to be used by ssh (partially reusing vagrant generated ssh keys and config)"
+echo "### Generating the list of machines which are up:"
 all_runningVagrMs=$(vagrant status | grep 'running (virtualbox)' | cut -d" " -f1)
+echo "   List of machines: $all_runningVagrMs" | tr '\n' ' ' 
+echo ""
+
+### 
+echo "### Creating list of machines with FQDN"
+runningVagrM_FQDN=""
+for runningVagrM in $all_runningVagrMs ; do
+  runningVagrM_FQDN="${runningVagrM_FQDN} `ping -c 1 ${runningVagrM} | head -1 |cut -d " " -f2`"
+done
+echo "  List of FQDN for the vagrant machines: $runningVagrM_FQDN"
+
+###
+echo "### (re)Generating a ssh_config to be used by ssh (partially reusing vagrant generated ssh keys and config)"
 rm -f ssh_config
 for runningVagrM in $all_runningVagrMs ; do
-  vagrant ssh-config $runningVagrM | sed "s|^ *HostName .*|  HostName $runningVagrM|g" | sed "s|^ *Port .*|  Port 22|g" >>ssh_config
+  vagrant ssh-config $runningVagrM | sed "s|^Host .*|Host ${runningVagrM}\*|g" | sed "/^ *HostName .*/d" | sed "s|^ *Port .*|  Port 22|g" >>ssh_config
 done
 
-echo "to ssh into any of the machines, run like this: "
+###
+#When below is enabled, ansible won't be able to run
+#echo "### update ~vagrant/.ssh/authorized_keys inside each machine to automatically switch to root user (instead of vagrant) "
+#set -vx
+#for runningVagrM in $all_runningVagrMs ; do
+#  ssh -F ./ssh_config $runningVagrM sed -i \'s#^ssh-#command=\"sudo -iu root\" ssh-#g\' ~vagrant/.ssh/authorized_keys
+#done
+#set +vx
+
+echo "  To ssh into any of the machines, run like this: "
 echo ""
-for M in ${all_runningVagrMs} ; do
+#for M in ${all_runningVagrMs} ; do
+for M in ${runningVagrM_FQDN} ; do
   echo "ssh -F ./ssh_config $M "
 done
 echo
@@ -89,17 +135,21 @@ become_method=sudo
 [ssh_connection]
 ssh_args = -C -o ControlMaster=auto -o ControlPersist=60s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -F ./ssh_config 
 EOF
-echo "a local ./ansible.cfg has been generated with success"
+echo "  a local ./ansible.cfg has been generated with success"
 echo
 
 ###
 echo "### Generating (guessing) inventory file based on host names (master must have word master in its name)"
 echo "[master]" > hosts
-echo $all_runningVagrMs | tr ' ' '\n' | grep "master" >> hosts
+#echo $all_runningVagrMs | tr ' ' '\n' | grep "master" >> hosts
+echo $runningVagrM_FQDN | tr ' ' '\n' | grep "master" >> hosts
 echo "[node]" >> hosts
-echo $all_runningVagrMs | tr ' ' '\n' | grep -v "master" >> hosts
+#echo $all_runningVagrMs | tr ' ' '\n' | grep -v "master" >> hosts
+echo $runningVagrM_FQDN | tr ' ' '\n' | grep -v "master" >> hosts
 echo
-echo "the prepared inventory (./hosts file) looks like this:"
+echo "  the prepared inventory (./hosts file) looks like this:"
 cat hosts
 
-echo "You may proceed reviewing group_vars/all and run ansible playbooks like site.yml (if not already done, before site.yml, run the docker install playbooks)"
+echo -e "You may proceed reviewing configuration with:\n vi group_vars/all \n and then run ansible playbooks like site.yml \n ansible-playbook -i hosts -v site.yml "
+
+
