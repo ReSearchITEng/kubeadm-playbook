8bc4f5ea07aae6864900ce6bf85834dac0d2f084
ReSearchITEng <ReSearchITEng@users.noreply.github.com>
2018-10-05 23:57:33 +0300

    typo

--- roles/storage/files/vsphere_bug_fix.sh
+++ roles/storage/files/vsphere_bug_fix.sh
@@ -3,8 +3,7 @@ export KUBECONFIG=/etc/kubernetes/admin.conf
 NeedRebootList=""
 for h in $(kubectl get nodes | tail -n +2 | awk '{print $1}'); do
   uuid=$(kubectl describe node/$h | grep -i UUID | tr '[:upper:]' '[:lower:]' | awk '{print $3}')
-  #eval kubectl patch node $h -p \'{\"spec\":{\"providerID\":\"vsphere://${uuid}\"}}\' | grep 'no change' >/dev/null
-  eval kubectl patch node $h -p \'{\"spec\":{\"providerID\":\"vsphere://${uuid}\"}}\' | grep 'no changed' >/dev/null
+  eval kubectl patch node $h -p \'{\"spec\":{\"providerID\":\"vsphere://${uuid}\"}}\' | grep 'no change' >/dev/null
   [[ $? -gt 0 ]] && NeedRebootList="$NeedRebootList $h"
 done
 [[ -n $NeedRebootList ]] && echo "$NeedRebootList" | tr ' ' '\n' | tail -n +2
