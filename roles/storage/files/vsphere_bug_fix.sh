dd0a759013eeab1b580859a9c07952524260c663
ReSearchITEng <ReSearchITEng@gmail.com>
2019-05-11 15:49:54 +0300

    1.14 - multimaster with official kubeadm solution

--- roles/storage/files/vsphere_bug_fix.sh
+++ roles/storage/files/vsphere_bug_fix.sh
@@ -4,10 +4,13 @@ NeedRebootList=""
 for h in $(kubectl get nodes | tail -n +2 | awk '{print $1}'); do
   uuid=$(kubectl describe node/$h | grep -i UUID | tr '[:upper:]' '[:lower:]' | awk '{print $3}')
   eval kubectl patch node $h -p \'{\"spec\":{\"providerID\":\"vsphere://${uuid}\"}}\' | grep 'no change' >/dev/null
-  [[ $? -gt 0 ]] && NeedRebootList="$NeedRebootList $h"
+  if [[ $? -gt 0 ]]; then
+    kubectl delete node $h  # As per vmware support suggetion: delete node and restart kubelet (see code: https://github.com/kubernetes/kubernetes/blob/v1.14.1/pkg/cloudprovider/providers/vsphere/vsphere.go#L278 )
+    NeedRebootList="$NeedRebootList $h"
+  fi
 done
 if [[ -n $NeedRebootList ]]; then
   echo "$NeedRebootList" | tr ' ' '\n' | tail -n +2
 fi
-### NeedRebootList holds the list of machines where there was a change and requrie reboot
+### NeedRebootList holds the list of machines where there was a change and requrie reboot (or maybe at least kubelet restart)
 
