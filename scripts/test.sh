ca65246779921714fcde4321d5938dda97b7b1f8
ReSearchITEng <ReSearchITEng@gmail.com>
2021-07-17 22:56:53 +0300

    1.21.3 cert

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,4 +7,8 @@ sudo chown -R `id -u`:`id -g` ./kubeadm-playbook
 cd ./kubeadm-playbook
 cp -p .././kubeadm-playbook.old/hosts .
 sed -i 's/myk8s.corp.example.com/ap/' group_vars/all/network.yml
+sudo kubeadm reset -f
 ansible-playbook -i hosts site.yml
+sudo cp -pf /etc/kubernetes/admin.conf ~/.kube/config
+sudo chown -R `id -u`:`id -g` ~/.kube/config
+
